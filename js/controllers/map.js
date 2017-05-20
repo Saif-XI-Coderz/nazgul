2524ca9d808d335712aec9476c0b5bf512f103e1
Saif-XI-Coderz <saifsohomow@gmail.com>
2017-05-20 23:42:37 +0400

    map zoom in

--- js/controllers/map.js
+++ js/controllers/map.js
@@ -1,31 +1,36 @@
 angular.module('al')
 
 .controller('MapCtrl', function($scope, $uibModalInstance, $document) {
+  $scope.currentState = 'zoom-initial';
   $scope.close = function() {
     $uibModalInstance.dismiss('cancel');
   };
-  function MapItem(bottom,right,imgClass,title,description){
+  function MapItem(bottom,right,imgClass,onClick,title,description){
     this.bottom=bottom;
     this.right=right;
     this.imgClass=imgClass;
+    this.onClick=onClick;
     this.title=title;
     this.description=description;
   }
   $scope.mapItems = [
-    new MapItem(100,10,"img-map-item",'Attraction 1','Cool description about attraction 1'),
-    new MapItem(10,159,"img-map-item",'Attraction 2','Cool description about attraction 2'),
-    new MapItem(58,60,"img-map-item",'Attraction 3','Cool description about attraction 3')
+    new MapItem('28.5%','70.5%',"img-map-item",'zoomWarriors','Warriors','Cool description about attraction 1. (click to zoom)'),
+    new MapItem('74%','44.5%',"img-map-item",'zoomSuperHero','Super Hero','Cool description about attraction 2. (click to zoom)'),
+    new MapItem('28%','20.5%',"img-map-item",'zoomHorror','Horror','Cool description about attraction 3. (click to zoom)')
   ];
-    $scope.mapItemsTo = [
-    new MapItem(150,60,"img-map-item",'Attraction 1','Cool description about attraction 1'),
-    new MapItem(60,209,"img-map-item",'Attraction 2','Cool description about attraction 2'),
-    new MapItem(108,110,"img-map-item",'Attraction 3','Cool description about attraction 3')
-  ];
-  $scope.mapItemsFrom = [
-    new MapItem(200,110,"img-map-item",'Attraction 1','Cool description about attraction 1'),
-    new MapItem(110,259,"img-map-item",'Attraction 2','Cool description about attraction 2'),
-    new MapItem(158,160,"img-map-item",'Attraction 3','Cool description about attraction 3')
-  ];
+
+  $scope.zoomSuperHero = function(){
+    $scope.currentState = 'zoom-in-super-hero';
+  };
+
+  $scope.zoomWarriors = function(){
+    $scope.currentState = 'zoom-in-warriors';
+  };
+
+  $scope.zoomHorror = function(){
+    $scope.currentState = 'zoom-in-horror';
+  };
+
   $document.ready(function () {
     $("[data-toggle='popover']").popover();
   });

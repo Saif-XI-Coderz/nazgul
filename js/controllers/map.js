05d0013cf3d43146f83dc81f3377118a9438bd42
Saif-XI-Coderz <saifsohomow@gmail.com>
2017-05-20 20:52:10 +0400

    map

--- js/controllers/map.js
+++ js/controllers/map.js
@@ -1,7 +1,32 @@
 angular.module('al')
 
-.controller('MapCtrl', function($scope, $uibModalInstance) {
+.controller('MapCtrl', function($scope, $uibModalInstance, $document) {
   $scope.close = function() {
     $uibModalInstance.dismiss('cancel');
+  };
+  function MapItem(bottom,right,imgClass,title,description){
+    this.bottom=bottom;
+    this.right=right;
+    this.imgClass=imgClass;
+    this.title=title;
+    this.description=description;
   }
-})
+  $scope.mapItems = [
+    new MapItem(100,10,"img-map-item",'Attraction 1','Cool description about attraction 1'),
+    new MapItem(10,159,"img-map-item",'Attraction 2','Cool description about attraction 2'),
+    new MapItem(58,60,"img-map-item",'Attraction 3','Cool description about attraction 3')
+  ];
+    $scope.mapItemsTo = [
+    new MapItem(150,60,"img-map-item",'Attraction 1','Cool description about attraction 1'),
+    new MapItem(60,209,"img-map-item",'Attraction 2','Cool description about attraction 2'),
+    new MapItem(108,110,"img-map-item",'Attraction 3','Cool description about attraction 3')
+  ];
+  $scope.mapItemsFrom = [
+    new MapItem(200,110,"img-map-item",'Attraction 1','Cool description about attraction 1'),
+    new MapItem(110,259,"img-map-item",'Attraction 2','Cool description about attraction 2'),
+    new MapItem(158,160,"img-map-item",'Attraction 3','Cool description about attraction 3')
+  ];
+  $document.ready(function () {
+    $("[data-toggle='popover']").popover();
+  });
+});
